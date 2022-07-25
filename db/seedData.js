42badee1a65d74a67432a556aafbc00861359ba6
BenSwartz <odanata410@gmail.com>
2022-07-25 10:23:40 -0500

    added activities routines routineactivities tables

--- db/seedData.js
+++ db/seedData.js
@@ -12,6 +12,8 @@ async function dropTables() {
     await client.query(`
     DROP TABLE IF EXISTS users;
     DROP TABLE IF EXISTS activities;
+    DROP TABLE IF EXISTS routines;
+    DROP TABLE IF EXISTS routine_activities;
     `);
   }
   catch (error) {
@@ -35,6 +37,21 @@ async function createTables() {
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL,
     description TEXT NOT NULL
+  );
+  CREATE TABLE routines (
+    id SERIAL PRIMARY KEY,
+    "creatorId" INTEGER REFERENCES users(id),
+    "isPublic" BOOLEAN DEFAULT false,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    goal TEXT NOT NULL
+  );
+  CREATE TABLE routine_activities (
+    id SERIAL PRIMARY KEY,
+    "routineId" INTEGER REFERENCES routines(id),
+    "activityId" INTEGER REFERENCES activities(id),
+    duration INTEGER,
+    count INTEGER,
+    UNIQUE ("routineId", "activityId")
   );
   `)
   }
