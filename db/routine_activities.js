ed6ef5bb7a156848aec6a0804f1c18412f99e4b0
BenSwartz <odanata410@gmail.com>
2022-07-26 10:47:49 -0500

    creating routines

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -1,21 +1,38 @@
+const { getActivityById } = require("./activities");
 const client = require("./client");
+const { getRoutineById } = require("./routines");
 
 async function addActivityToRoutine({
   routineId,
   activityId,
   count,
   duration,
-}) {}
+}) {
+  try {
 
-async function getRoutineActivityById(id) {}
+    const rows = await client.query(
+      `INSERT INTO routine_activities(routineId, activityId, count, duration)
+     VALUES ($1, $2, $3, $4) 
+     RETURNING *;`,
+      [routineId, activityId, count, duration]
+    );
 
-async function getRoutineActivitiesByRoutine({ id }) {}
+    console.log(rows)
+    return rows
+  } catch (error) {
+    console.error("Failed to add activity to routine")
+  }
+}
 
-async function updateRoutineActivity({ id, ...fields }) {}
+async function getRoutineActivityById(id) { }
 
-async function destroyRoutineActivity(id) {}
+async function getRoutineActivitiesByRoutine({ id }) { }
 
-async function canEditRoutineActivity(routineActivityId, userId) {}
+async function updateRoutineActivity({ id, ...fields }) { }
+
+async function destroyRoutineActivity(id) { }
+
+async function canEditRoutineActivity(routineActivityId, userId) { }
 
 module.exports = {
   getRoutineActivityById,
--- db/routines.js
+++ db/routines.js
@@ -1,26 +1,40 @@
 const client = require("./client");
 
-async function createRoutine({ creatorId, isPublic, name, goal }) {}
+async function createRoutine({ creatorId, isPublic, name, goal }) {
+  try {
+    const { rows } = await client.query(
+      `INSERT INTO routines("creatorId", "isPublic", name, goal)
+       VALUES ($1, $2, $3, $4) 
+       RETURNING *;`,
+      [creatorId, isPublic, name, goal]
+    );
+    console.log(rows[0], 'New Routine')
+    return rows[0];
+  } catch (error) {
+    console.error("failed to create routine!")
+    throw error;
+  }
+}
 //first branch routines
 async function getRoutineById(id) {
-  
+
 }
 
-async function getRoutinesWithoutActivities() {}
+async function getRoutinesWithoutActivities() { }
 
-async function getAllRoutines() {}
+async function getAllRoutines() { }
 
-async function getAllPublicRoutines() {}
+async function getAllPublicRoutines() { }
 
-async function getAllRoutinesByUser({ username }) {}
+async function getAllRoutinesByUser({ username }) { }
 
-async function getPublicRoutinesByUser({ username }) {}
+async function getPublicRoutinesByUser({ username }) { }
 
-async function getPublicRoutinesByActivity({ id }) {}
+async function getPublicRoutinesByActivity({ id }) { }
 
-async function updateRoutine({ id, ...fields }) {}
+async function updateRoutine({ id, ...fields }) { }
 
-async function destroyRoutine(id) {}
+async function destroyRoutine(id) { }
 
 module.exports = {
   getRoutineById,
