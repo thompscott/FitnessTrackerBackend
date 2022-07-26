a42495b6efceedd08898e1d9f343e0cfb8d24478
BenSwartz <odanata410@gmail.com>
2022-07-26 14:29:23 -0500

    getting all routines

--- db/routines.js
+++ db/routines.js
@@ -2,7 +2,7 @@ const client = require("./client");
 
 async function createRoutine({ creatorId, isPublic, name, goal }) {
   try {
-    const  { rows } = await client.query(
+    const { rows } = await client.query(
       `INSERT INTO routines("creatorId", "isPublic", name, goal)
        VALUES ($1, $2, $3, $4) 
        RETURNING *;`,
@@ -23,13 +23,16 @@ async function getRoutinesWithoutActivities() { }
 
 async function getAllRoutines() {
   try {
+
     const { rows } = await client.query(
       `
-      SELECT *
-      FROM routines;
+      SELECT * 
+      FROM routines
+      FULL JOIN users
+      ON routines."creatorId" = users.id;
     `
     );
-
+    console.log(rows, "all of our routines")
     return rows;
   } catch (error) {
     console.error("failed to get routines!");
