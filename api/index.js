4b7b8a53b944b1e860020c26441d665f457668ad
thompscott <tho.sco.lisd@gmail.com>
2022-07-27 23:20:48 -0500

    Removed cors listener from app.js to remove port in use error

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const { createUser, getUserByUsername } = require('..db/');
 const router = express.Router();
--- app.js
+++ app.js
@@ -11,8 +11,5 @@ app.get('/', function (req, res, next) {
     res.json({ msg: 'This is CORS-enabled for all origins!' })
 })
 
-app.listen(3001, function () {
-    console.log('CORS-enabled web server listening on port 3001')
-})
 
 module.exports = app;
