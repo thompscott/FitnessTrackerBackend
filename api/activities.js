f379767b219e6100db5a8b8ce27b4ebf39ef5e5a
BenSwartz <odanata410@gmail.com>
2022-07-28 16:56:11 -0500

    finished activities routes problem on one

--- api/activities.js
+++ api/activities.js
@@ -1,12 +1,66 @@
 const express = require('express');
+const { getAllActivities, createActivity, updateActivity, getActivityByName, getActivityById, getPublicRoutinesByActivity } = require('../db');
 const activitiesRouter = express.Router();
 
 // GET /api/activities/:activityId/routines
+activitiesRouter.get("/:activityId/routines", async (req, res, next) => {
+    try {
+        if (! await getActivityById(req.params.activityId)) {
+            res.send({ error: "activity error", message: `Activity ${req.params.activityId} not found`, name: "existingactivityerror" })
+        }
+        // Problem req.params don't work
+        const actId = req.params.activityId
+        console.log(actId, "this is our activity")
+        const activities = await getPublicRoutinesByActivity({ id: actId })
+        // console.log(activities, "this is our activity")
+        res.send(activities)
+    } catch (error) {
+        next({ error: error.error, message: error.message, name: error.name })
+    }
+})
 
 // GET /api/activities
+activitiesRouter.get("/", async (req, res, next) => {
+    try {
+        const activities = await getAllActivities()
+        res.send(activities)
+    } catch (error) {
+        next({ error: error.error, message: error.message, name: error.name })
+    }
+})
 
 // POST /api/activities
+activitiesRouter.post("/", async (req, res, next) => {
+    try {
+        const activities = await createActivity({ name: req.body.name, description: req.body.description })
+        if (activities) {
+            res.send(activities)
+        } else {
+            res.send({ error: "activity error", message: `An activity with name ${req.body.name} already exists`, name: "existingactivityerror" })
+        }
+
+    } catch (error) {
+        next({ error: error.error, message: error.message, name: error.name })
+    }
+})
 
 // PATCH /api/activities/:activityId
+activitiesRouter.patch("/:activityId", async (req, res, next) => {
+    try {
+        const name = req.body.name
+        const description = req.body.description
+        if (! await getActivityById(req.params.activityId)) {
+            res.send({ error: "activity error", message: `Activity ${req.params.activityId} not found`, name: "existingactivityerror" })
+        }
+        if (await getActivityByName(name)) {
+            res.send({ error: "activity error", message: `An activity with name ${name} already exists`, name: "existingactivityerror" })
+        }
+        const activities = await updateActivity({ id: req.params.activityId, name, description })
+        res.send(activities)
+
+    } catch (error) {
+        next({ error: error.error, message: error.message, name: error.name })
+    }
+})
 
 module.exports = activitiesRouter;
